ca868b405f34cd0ebcfdc268c5f315580e4b8106
deuteridayo <smrf.hnhn@gmail.com>
2023-05-25 20:55:46 +0900

    first commit

---
+++ src/layouts/MainLayout.vue
@@ -0,0 +1,83 @@
+<template>
+  <q-layout view="lHh Lpr lFf">
+    <q-header elevated>
+      <q-toolbar class="bg-light-blue-4">
+        <q-btn
+          flat
+          dense
+          round
+          icon="menu"
+          aria-label="Menu"
+          @click="toggleLeftDrawer"
+        />
+
+        <q-toolbar-title> じゅーすいそのホームページ </q-toolbar-title>
+
+        <div>Quasar v{{ $q.version }}</div>
+      </q-toolbar>
+    </q-header>
+
+    <q-drawer v-model="leftDrawerOpen" show-if-above bordered>
+      <q-list>
+        <q-item-label header> Essential Links </q-item-label>
+
+        <EssentialLink
+          v-for="link in essentialLinks"
+          :key="link.title"
+          v-bind="link"
+        />
+      </q-list>
+    </q-drawer>
+
+    <q-page-container>
+      <router-view />
+    </q-page-container>
+  </q-layout>
+</template>
+
+<script lang="ts">
+import { defineComponent, ref } from 'vue';
+import EssentialLink from 'components/EssentialLink.vue';
+
+const linksList = [
+  {
+    title: 'AtCoder',
+    caption: 'deuteridayo',
+    icon: 'trending_up',
+    link: 'https://atcoder.jp/users/deuteridayo',
+  },
+  {
+    title: 'Twitter',
+    caption: '@deuteridayodayo',
+    icon: 'chat_bubble_outline',
+    link: 'https://twitter.com/deuteridayodayo',
+  },
+
+  {
+    title: 'Github',
+    caption: 'github.com/deuteridayo',
+    icon: 'code',
+    link: 'https://github.com/deuteridayo',
+  },
+];
+
+export default defineComponent({
+  name: 'MainLayout',
+
+  components: {
+    EssentialLink,
+  },
+
+  setup() {
+    const leftDrawerOpen = ref(false);
+
+    return {
+      essentialLinks: linksList,
+      leftDrawerOpen,
+      toggleLeftDrawer() {
+        leftDrawerOpen.value = !leftDrawerOpen.value;
+      },
+    };
+  },
+});
+</script>

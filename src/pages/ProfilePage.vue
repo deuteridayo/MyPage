b6bec614751cd4990326deb75023c9256a01eec1
deuteridayo <smrf.hnhn@gmail.com>
2023-05-27 11:59:41 +0900

    プロフィール加筆

--- src/pages/ProfilePage.vue
+++ src/pages/ProfilePage.vue
@@ -12,16 +12,23 @@
       <br />
       <div class="row text-h6 q-ml-lg"><u>すきなもの</u></div>
       <div class="row text-h5 q-ml-lg">
-        競技プログラミング(AtCoder A:青/H:水)
+        競技プログラミング(AtCoder A: 青/H:水)
       </div>
-      <div class="row text-h5 q-ml-lg">サンリオ (シナモロール)</div>
+      <div class="row text-h5 q-ml-lg">サンリオ 特にシナモロール</div>
       <div class="row text-h5 q-ml-lg">ネイル</div>
-      <div class="row text-h5 q-ml-lg">かわいい服</div>
+      <div class="row text-h5 q-ml-lg">かわいい服 Amavelとか</div>
       <div class="row text-h5 q-ml-lg">μ's (ラブライブ！)</div>
       <div class="row text-h5 q-ml-lg">内田彩さん</div>
 
       <br />
       <div class="row text-h6 q-ml-lg"><u>持ってる資格など</u></div>
+      <div class="row text-h5 q-ml-lg">全国珠算教育連盟珠算検定 二段</div>
+      <div class="row text-h5 q-ml-lg">
+        アルゴリズム実技検定 上級（〜2025年5月）
+      </div>
+      <div class="row text-h5 q-ml-lg text-purple">
+        応用情報受かってるといいな
+      </div>
     </div>
   </q-page>
 </template>
